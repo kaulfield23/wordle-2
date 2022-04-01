1e2e6c3caac541deef636f8bc8d2c8d8a16ad261
kaulfield23 <kaulfield23@gmail.com>
2022-04-01 17:25:02 +0200

    styled highscore page

--- server/server.js
+++ server/server.js
@@ -3,14 +3,39 @@ import api from './router/api.js'
 import {
     engine
 } from 'express-handlebars';
+
 import path from "path"
+import {
+    resourceLimits
+} from 'worker_threads';
 const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json())
-app.engine('handlebars', engine());
-
+    // app.engine('handlebars', engine());
+app.engine('handlebars', engine({
+    helpers: {
+        math: function(lvalue, operator, rvalue) {
+            lvalue = parseFloat(lvalue);
+            rvalue = parseFloat(rvalue);
+            return {
+                "+": lvalue + rvalue,
+                "-": lvalue - rvalue,
+                "*": lvalue * rvalue,
+                "/": lvalue / rvalue,
+                "%": lvalue % rvalue
+            }[operator];
+        },
+        strFunc: function(word) {
+            let result = '';
+            for (let i = 0; i < word.length; i++) {
+                result += `<h3 class="rightWordsBox">${word[i]}</h3>`
+            }
+            return result
+        }
+    }
+}));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/server/templates/`);
 app.use('/api', api);
